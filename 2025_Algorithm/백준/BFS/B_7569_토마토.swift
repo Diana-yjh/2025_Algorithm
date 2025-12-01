d4f18a67ac47ae514e9fe0dac291967910b2345e
Diana-yjh <projin1234@gmail.com>
2025-12-01 10:43:18 +0900

    FEAT :: 백준 11501

---
+++ 2025_Algorithm/백준/BFS/B_7569_토마토.swift
@@ -0,0 +1,10 @@
+//
+//  B_7569_토마토.swift
+//  2025_Algorithm
+//
+//  Created by Yejin Hong on 12/1/25.
+//
+
+struct B_7569_토마토 {
+    
+}
--- 2025_Algorithm/백준/그리디알고리즘/B_11501_주식.swift
+++ 2025_Algorithm/백준/그리디알고리즘/B_11501_주식.swift
@@ -4,5 +4,32 @@
 //
 //  Created by Yejin Hong on 11/24/25.
 //
-
 import Foundation
+
+struct B_11501_주식 {
+    func solution() {
+        let T = Int(readLine()!)!
+        
+        (1...T).forEach { _ in
+            let days = Int(readLine()!)!
+            let stocks = readLine()!.split(separator: " ").map{ Int($0)! }
+            var maxNum = 0
+            var gains: [Int] = []
+            var result = 0
+            
+            for day in stride(from: days-1, to: -1, by: -1) {
+                maxNum = max(maxNum, stocks[day])
+                
+                gains.append(maxNum)
+            }
+            
+            gains.reverse()
+            
+            for i in 0 ..< days {
+                result += gains[i] - stocks[i]
+            }
+            
+            print(result)
+        }
+    }
+}
