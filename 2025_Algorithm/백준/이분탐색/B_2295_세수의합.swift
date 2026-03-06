cdc93cedf2e83bce22dae75c02ee455ad18ddeb9
Diana-yjh <projin1234@gmail.com>
2026-03-06 16:08:41 +0900

    FEAT :: 백준 2295

--- 2025_Algorithm/백준/이분탐색/B_2295_세수의합.swift
+++ 2025_Algorithm/백준/이분탐색/B_2295_세수의합.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 struct B_2295_세수의합 {
-    func solution() {}
+    func solution() {
+        let n = Int(readLine()!)!
+        var u: [Int] = (1...n).map{ _ in Int(readLine()!)! }
+        
+        u.sort(by: >)
+        
+        for a in 0..<u.count {
+            for i in a+1..<u.count {
+                var left = i
+                var right = u.count - 1
+                
+                while left <= right {
+                    if u[left] + u[right] < u[a] - u[i] {
+                        right -= 1
+                    } else if u[left] + u[right] > u[a] - u[i] {
+                        left += 1
+                    } else {
+                        print(u[a])
+                        return
+                    }
+                }
+            }
+        }
+    }
 }
